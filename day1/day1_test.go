98d6f61fc3ee18c651cebe1cf63b966d55817455
mgdarroch <mgdarroch@gmail.com>
2023-12-01 12:02:43 +0000

    first test case for part 2, these flipping elves

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+// PART 1
+
 func TestParseLineFindsTheFirstAndLastNumbersInALineWithTwoNumbers(t *testing.T) {
 	t.Parallel()
 	str := "1abc2"
@@ -58,6 +60,31 @@ func TestSumLinesWillAddAllResults(t *testing.T) {
 	for _, line := range lines {
 		got = day1.SumLines(got, line)
 	}
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+// PART 2
+/*
+
+INPUT
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+*/
+
+func TestParseLineCanFindValidDigitStrings(t *testing.T) {
+	t.Parallel()
+	line := "two1nine"
+	want := 29
+	got := day1.ParseLine(line)
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
