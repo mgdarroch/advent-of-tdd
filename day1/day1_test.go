a061c30f3dcc8e7c6eec9a7c9d9d15b5c275fae7
mgdarroch <mgdarroch@gmail.com>
2023-12-01 11:30:40 +0000

    expanding the test cases, adding failing test for a string with only one number.

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -5,10 +5,45 @@ import (
 	"testing"
 )
 
-func TestFindsTheFirstAndLastNumber(t *testing.T) {
+//1abc2
+//pqr3stu8vwx
+//a1b2c3d4e5f
+//treb7uchet
+
+func TestFindsTheFirstAndLastNumbersInALineWithTwoNumbers(t *testing.T) {
 	t.Parallel()
 	str := "1abc2"
 	want := 12
+	got := day1.ParseLine(str)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestFindsTheFirstAndLastNumbersInALineWithTwoNumbersWithCharactersAtTheStart(t *testing.T) {
+	t.Parallel()
+	str := "pqr3stu8vwx"
+	want := 38
+	got := day1.ParseLine(str)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestFindsTheFirstAndLastNumbersWithMultipleNumbers(t *testing.T) {
+	t.Parallel()
+	str := "a1b2c3d4e5f"
+	want := 15
+	got := day1.ParseLine(str)
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestFindsTheFirstAndLastNumbersWithASingleNumber(t *testing.T) {
+	t.Parallel()
+	str := "treb7uchet"
+	want := 77
 	got := day1.ParseLine(str)
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
