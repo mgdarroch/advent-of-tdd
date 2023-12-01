45e34b5b9cab808432bc31fcd1f992f6233c6f83
mgdarroch <mgdarroch@gmail.com>
2023-12-01 17:46:17 +0000

    refactoring main.go to wrap the solution execution in a function

--- main.go
+++ main.go
@@ -9,20 +9,20 @@ import (
 )
 
 func main() {
+	solutionDay1()
+}
+
+func solutionDay1() {
 	f, err := os.Open("day1/resources/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer f.Close()
-
 	scanner := bufio.NewScanner(f)
 	answer := 0
-
 	for scanner.Scan() {
 		line := scanner.Text()
 		answer = day1.SumLines(answer, line)
 	}
-
 	fmt.Println(answer)
 }
