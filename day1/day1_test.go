4cab0fae807fe27ffdf044b84d2e0679af2cede2
mgdarroch <mgdarroch@gmail.com>
2023-12-01 11:13:42 +0000

    getting java out of my system and setting the parseLine method to be the public ParseLine

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -9,7 +9,7 @@ func TestFindsTheFirstAndLastNumber(t *testing.T) {
 	t.Parallel()
 	str := "1abc2"
 	want := 12
-	got := day1.parseLine(str)
+	got := day1.ParseLine(str)
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
