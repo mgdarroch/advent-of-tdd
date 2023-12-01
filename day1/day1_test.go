8a7a9f623bde4064059b8bc2a0efd07f38d109dd
mgdarroch <mgdarroch@gmail.com>
2023-12-01 11:46:21 +0000

    updating the test

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -5,11 +5,6 @@ import (
 	"testing"
 )
 
-//1abc2
-//pqr3stu8vwx
-//a1b2c3d4e5f
-//treb7uchet
-
 func TestParseLineFindsTheFirstAndLastNumbersInALineWithTwoNumbers(t *testing.T) {
 	t.Parallel()
 	str := "1abc2"
@@ -53,7 +48,16 @@ func TestParseLineFindsTheFirstAndLastNumbersWithASingleNumber(t *testing.T) {
 func TestSumLinesWillAddAllResults(t *testing.T) {
 	t.Parallel()
 	want := 142
-	got := day1.SumLines("1abc2")
+	got := 0
+	lines := [4]string{
+		"1abc2",
+		"pqr3stu8vwx",
+		"a1b2c3d4e5f",
+		"treb7uchet",
+	}
+	for _, line := range lines {
+		got = day1.SumLines(got, line)
+	}
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
