582cb7bbf2224f278560a0c34350334d6850e04c
mgdarroch <mgdarroch@gmail.com>
2023-12-02 14:17:32 +0000

    adding new test to keep track of the sum of powers across multiple inputs, requires a refactoring of the ValidateGame method

--- day2/day2_test.go
+++ day2/day2_test.go
@@ -117,7 +117,7 @@ func TestValidateGameShouldReturnIncrementedSumIfGameIsValid(t *testing.T) {
 	t.Parallel()
 	want := 1
 	line := "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-	gotSum, _ := day2.ValidateGame(0, line)
+	gotSum, _ := day2.ValidateGame(line, 0, 0)
 	if want != gotSum {
 		t.Errorf("want %d, got %d", want, gotSum)
 	}
@@ -127,7 +127,7 @@ func TestValidateGameShouldReturnSameSumIfGameIsInvalid(t *testing.T) {
 	t.Parallel()
 	want := 0
 	line := "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-	got, _ := day2.ValidateGame(0, line)
+	got, _ := day2.ValidateGame(line, 0, 0)
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
@@ -137,13 +137,13 @@ func TestValidateGameShouldReturnSameSumIfGameIsInvalidMoreThanThreeSessions(t *
 	t.Parallel()
 	want := 0
 	line := "Game 11: 2 red, 18 blue, 5 green; 4 green, 12 blue, 9 red; 6 red, 4 green, 5 blue; 8 red, 16 blue, 2 green; 1 green, 18 blue, 13 red; 13 blue, 9 red"
-	got, _ := day2.ValidateGame(0, line)
+	got, _ := day2.ValidateGame(line, 0, 0)
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
 	}
 }
 
-func TestValidateGameCalledOverExampleInputReturns8(t *testing.T) {
+func TestValidateGameCalledOverExampleInputReturnsCorrectIdSum(t *testing.T) {
 	t.Parallel()
 	lines := [5]string{
 		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
@@ -155,7 +155,7 @@ func TestValidateGameCalledOverExampleInputReturns8(t *testing.T) {
 	want := 8
 	got := 0
 	for _, line := range lines {
-		got, _ = day2.ValidateGame(got, line)
+		got, _ = day2.ValidateGame(line, got, 0)
 	}
 	if want != got {
 		t.Errorf("want %d, got %d", want, got)
@@ -166,7 +166,7 @@ func TestValidateGameReturnsTheSumOfThePowerOfMinimumCubesTotal48(t *testing.T) 
 	t.Parallel()
 	wantPower := 48
 	line := "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
-	_, gotPower := day2.ValidateGame(0, line)
+	_, gotPower := day2.ValidateGame(line, 0, 0)
 	if wantPower != gotPower {
 		t.Errorf("want %d, got %d", wantPower, gotPower)
 	}
@@ -176,8 +176,27 @@ func TestValidateGameReturnsTheSumOfThePowerOfMinimumCubesTotal1560(t *testing.T
 	t.Parallel()
 	wantPower := 1560
 	line := "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
-	_, gotPower := day2.ValidateGame(0, line)
+	_, gotPower := day2.ValidateGame(line, 0, 0)
 	if wantPower != gotPower {
 		t.Errorf("want %d, got %d", wantPower, gotPower)
+	}
+}
+
+func TestValidateGameCalledOverExampleInputReturnsCorrectPowerSum(t *testing.T) {
+	t.Parallel()
+	lines := [5]string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
+	want := 2286
+	got := 0
+	for _, line := range lines {
+		_, got = day2.ValidateGame(line, 0, got)
+	}
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
 	}
 }
