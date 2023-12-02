440c5bd73d75d1b4ec2aefe7e711bbed82d4a7a0
mgdarroch <mgdarroch@gmail.com>
2023-12-02 12:01:15 +0000

    extracting the gameId programmatically, does not catch two digit integers.

--- day2/day2.go
+++ day2/day2.go
@@ -2,12 +2,17 @@ package day2
 
 import (
 	"errors"
+	"strconv"
 	"strings"
 )
 
 func ParseLine(input string) (int, error) {
+	var gameId int
+	if input[6:7] == ":" {
+		gameId, _ = strconv.Atoi(input[5:6])
+	}
 	if strings.Contains(input, "Game 1") {
-		return 1, nil
+		return gameId, nil
 	} else {
 		return 0, errors.New("invalid game")
 	}
