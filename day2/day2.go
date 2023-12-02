b2eabc016c6e0336946017b3efef5888744e6f0e
mgdarroch <mgdarroch@gmail.com>
2023-12-02 11:52:47 +0000

    tests now passing, time to refactor

--- day2/day2.go
+++ day2/day2.go
@@ -1,1 +1,14 @@
 package day2
+
+import (
+	"errors"
+	"strings"
+)
+
+func ParseLine(input string) (int, error) {
+	if strings.Contains(input, "Game 1") {
+		return 1, nil
+	} else {
+		return 0, errors.New("invalid game")
+	}
+}
