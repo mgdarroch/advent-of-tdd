8e8e98dbe791ae298fe90382ff810185ab1c4086
mgdarroch <mgdarroch@gmail.com>
2023-12-02 12:03:55 +0000

    pulling the gameId extraction out into a separate method, tests still passing.

--- day2/day2.go
+++ day2/day2.go
@@ -7,15 +7,20 @@ import (
 )
 
 func ParseLine(input string) (int, error) {
+	gameId := extractGameId(input)
+	if strings.Contains(input, "Game 1") {
+		return gameId, nil
+	} else {
+		return 0, errors.New("invalid game")
+	}
+}
+
+func extractGameId(input string) int {
 	var gameId int
 	if input[6:7] == ":" {
 		gameId, _ = strconv.Atoi(input[5:6])
 	} else {
 		gameId, _ = strconv.Atoi(input[5:7])
 	}
-	if strings.Contains(input, "Game 1") {
-		return gameId, nil
-	} else {
-		return 0, errors.New("invalid game")
-	}
+	return gameId
 }
