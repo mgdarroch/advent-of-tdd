c3530492268b55bbc0d80b1e831eca80e9105c24
mgdarroch <mgdarroch@gmail.com>
2023-12-02 12:02:58 +0000

    adding a conidition to extract double digit ids, test is now passing.

--- day2/day2.go
+++ day2/day2.go
@@ -10,6 +10,8 @@ func ParseLine(input string) (int, error) {
 	var gameId int
 	if input[6:7] == ":" {
 		gameId, _ = strconv.Atoi(input[5:6])
+	} else {
+		gameId, _ = strconv.Atoi(input[5:7])
 	}
 	if strings.Contains(input, "Game 1") {
 		return gameId, nil
