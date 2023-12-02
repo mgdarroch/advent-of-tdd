2551c6b66ee9f4aa3e937e9a9dbda0ec9fed9a65
mgdarroch <mgdarroch@gmail.com>
2023-12-02 13:42:33 +0000

    adding test to test the example input

--- day2/day2_test.go
+++ day2/day2_test.go
@@ -89,7 +89,7 @@ func TestValidateGameShouldReturnIncrementedSumIfGameIsValid(t *testing.T) {
 	line := "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
 	got := day2.ValidateGame(0, line)
 	if want != got {
-		t.Errorf("expected %d, got %d", want, got)
+		t.Errorf("want %d, got %d", want, got)
 	}
 }
 
@@ -99,6 +99,24 @@ func TestValidateGameShouldReturnSameSumIfGameIsInvalid(t *testing.T) {
 	line := "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
 	got := day2.ValidateGame(0, line)
 	if want != got {
-		t.Errorf("expected %d, got %d", want, got)
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestValidateGameCalledOverExampleInputReturns8(t *testing.T) {
+	lines := [5]string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
+	want := 8
+	got := 0
+	for _, line := range lines {
+		got = day2.ValidateGame(got, line)
+	}
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
 	}
 }
