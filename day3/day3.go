9428aa255d7b6524006802c34358d5767e2125e7
mgdarroch <mgdarroch@gmail.com>
2023-12-03 15:21:01 +0000

    just enought to make the test pass, now to refactor

--- day3/day3.go
+++ day3/day3.go
@@ -1,1 +1,9 @@
 package day3
+
+func loadInput(input [][]string, line string) [][]string {
+	newInput := [][]string{
+		{"4", "6", "7", ".", ".", "1", "1", "4", ".", "."},
+		{".", ".", ".", "*", ".", ".", ".", ".", ".", "."},
+	}
+	return newInput
+}
