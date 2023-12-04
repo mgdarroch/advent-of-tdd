c734d727b7f944370c28d2505953a9de584f186b
mgdarroch <mgdarroch@gmail.com>
2023-12-04 18:23:19 +0000

    test now passing

--- day4/day4.go
+++ day4/day4.go
@@ -1,15 +1,26 @@
 package day4
 
+import (
+	"strconv"
+)
+
 func buildNumberMap(input string) map[int]int {
-	return map[int]int{
-		41: 1,
-		48: 2,
-		83: 2,
-		86: 2,
-		17: 2,
-		6:  1,
-		31: 1,
-		9:  1,
-		53: 1,
+	numberMap := map[int]int{}
+	trimStr := input[8:]
+	foundNum := 0
+	for i := 0; i < len(trimStr); i++ {
+		num, err := strconv.Atoi(string(trimStr[i]))
+		if err == nil {
+			foundNum = (foundNum * 10) + num
+		} else {
+			if foundNum != 0 {
+				numberMap[foundNum] = numberMap[foundNum] + 1
+				foundNum = 0
+			}
+		}
 	}
+	if foundNum != 0 {
+		numberMap[foundNum] = numberMap[foundNum] + 1
+	}
+	return numberMap
 }
