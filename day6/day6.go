30dfff8c8b8554f58335620d500a37e9ec476ed6
mgdarroch <mgdarroch@gmail.com>
2023-12-09 23:27:33 +0000

    passing test

--- day6/day6.go
+++ day6/day6.go
@@ -1,23 +1,48 @@
 package day6
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Race struct {
 	Time     int
 	Distance int
 }
 
 func loadInput(input string) []Race {
-	return []Race{
-		{
-			Time:     7,
-			Distance: 9,
-		},
-		{
-			Time:     15,
-			Distance: 40,
-		},
-		{
-			Time:     30,
-			Distance: 200,
-		},
+	f, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+
+		}
+	}(f)
+
+	var races []Race
+	var lines []string
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	timeNumbers := strings.Fields(lines[0][11:])
+	distanceNumbers := strings.Fields(lines[1][11:])
+
+	for i := 0; i < len(timeNumbers); i++ {
+		timeNum, _ := strconv.Atoi(timeNumbers[i])
+		distanceNum, _ := strconv.Atoi(distanceNumbers[i])
+		races = append(races, Race{
+			Time:     timeNum,
+			Distance: distanceNum,
+		})
+	}
+	return races
 }
