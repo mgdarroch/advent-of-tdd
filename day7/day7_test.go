151ad857d6c7a26cf61ab340cdf00b2a45ec79bb
mgdarroch <mgdarroch@gmail.com>
2023-12-10 15:17:56 +0000

    first test to load input

--- day7/day7_test.go
+++ day7/day7_test.go
@@ -1,1 +1,36 @@
 package day7
+
+import (
+	"github.com/google/go-cmp/cmp"
+	"testing"
+)
+
+func TestLoadInputToRoundStruct(t *testing.T) {
+	input := "resources/input_test.txt"
+	want := []Round{
+		{
+			Hand: "32T3K",
+			Bid: 765,
+		},
+		{
+			Hand: "T55J5",
+			Bid: 684,
+		},
+		{
+			Hand: "KK677",
+			Bid: 28
+		},
+		{
+			Hand: "KTJJT",
+			Bid: 220
+		},
+		{
+			Hand: "QQQJA",
+			Bid: 483
+		},
+	}
+	got := loadInput(input)
+	if !cmp.Equal(want, got) {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
